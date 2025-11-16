94cf6b7023d20fb4910bf208dfdd73e8133bdfa7
812anuj <160117384+812anuj@users.noreply.github.com>
2025-11-16 12:49:38 +0530

    project created.

---
+++ src/main/java/com/example/DoctorDesk/DoctorDeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoctorDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoctorDesk/DoctorDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoctorDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
